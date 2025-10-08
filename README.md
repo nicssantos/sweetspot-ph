d8412be43ff816893687f7dd1141091ba62d7ef1
nicssantos <nicssantos04@gmail.com>
2025-10-08 23:35:58 +0400

    collect shop info

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 "# sweetspot-ph" 
 
 AI generated:
-woman on homepage
-icon
+woman on homepage, icon
+
+links:
+nanay nidas: https://wanderlog.com/place/details/9636249/nanay-nidas-homemade-leche-flan  
+bebang: https://www.instagram.com/p/CnnwDEIv4TN  
+lola nenas photo: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.philstar.com%2Flifestyle%2Fbusiness-life%2F2024%2F09%2F17%2F2385958%2Flola-nenas-co-founder-apologizes-working-lunch-video&psig=AOvVaw3hpAZwrSd8-yh1O6fcmakz&ust=1760037367366000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOCah6OolZADFQAAAAAdAAAAABAE  
---
+++ js/findAShop.js
@@ -0,0 +1,44 @@
+const shops = [
+    {
+        name: "Nanay Nidas Homemade Leche Flan",
+        image: "../img/shops/nanay-nidas.jpg",
+        description: "A hidden gem offering exceptional quality desserts at very reasonable prices.",
+        address: "826 Vicente Cruz St, Sampaloc, Manila, 1008 Metro Manila, Philippines",
+        mapLink: "https://maps.app.goo.gl/rKLUNvPTUG3UeTBj7"
+    },
+    {
+        name: "Bebang Halo Halo",
+        image: "../img/shops/bebang.png",
+        description: "A Filipino dessert chain known for its creamy Halo-Halo, made by freezing and shaving milk instead of ice.",
+        address: "Has multiple stores in Metro Manila, Philippines",
+        mapLink: ""
+    },
+    {
+        name: "Lola Nena's",
+        image: "../img/shops/",
+        description: "A Filipino-inspired bakery and cafe that brings the warmth and comfort of homemade goodness to its customers",
+        address: "Has multiple stores across the Philippines",
+        mapLink: ""
+    },
+    {
+        name: "",
+        image: "../img/shops/",
+        description: "",
+        address: "",
+        mapLink: ""
+    },
+    {
+        name: "",
+        image: "../img/shops/",
+        description: "",
+        address: "",
+        mapLink: ""
+    },
+    {
+        name: "",
+        image: "../img/shops/",
+        description: "",
+        address: "",
+        mapLink: ""
+    }
+]
